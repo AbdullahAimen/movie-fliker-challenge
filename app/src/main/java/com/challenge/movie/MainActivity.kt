a1799ab3d665710f60888d222d1b31e4581906ec
Abdullah Ayman <abaa@calldorado.com>
2021-03-13 19:02:13 +0200

    implement expandable list view

--- app/src/main/java/com/challenge/movie/MainActivity.kt
+++ app/src/main/java/com/challenge/movie/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
+import com.challenge.movie.adapters.ExpandableListAdapter
 import com.challenge.movie.adapters.MovieAdapter
+import com.challenge.movie.commands.SearchCommand
 import com.challenge.movie.viewModels.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.flow.collectLatest
@@ -22,10 +26,43 @@ class MainActivity : AppCompatActivity() {
         //creating adapter
         val adapter = MovieAdapter()
         main_recycler_movies.adapter = adapter
-
         //subscribe adapter to viewModel
         lifecycleScope.launch {
             mMainViewModel.allMovies.collectLatest { adapter.submitData(it) }
         }
+
+        main_searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query.isNullOrEmpty()) {
+                    main_expandableList.visibility = View.GONE
+                    return true
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText.isNullOrEmpty()) {
+                    main_expandableList.visibility = View.GONE
+                    return true
+                }
+                mMainViewModel.doSearch(newText ?: "")
+                return false
+            }
+
+        })
+        mMainViewModel.mSearchCommand.observe(this, Observer {
+            it.getContentIfNotHandled()?.let { it ->
+                when (it) {
+                    is SearchCommand.SubmitData -> {
+                        val mExpandableListAdapter = ExpandableListAdapter(this, it.datMap)
+                        main_expandableList.setAdapter(mExpandableListAdapter)
+                        if (it.datMap.isNotEmpty())
+                            main_expandableList.visibility = View.VISIBLE
+                        else
+                            main_expandableList.visibility = View.GONE
+                    }
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/challenge/movie/viewModels/MainViewModel.kt
+++ app/src/main/java/com/challenge/movie/viewModels/MainViewModel.kt
@@ -2,13 +2,17 @@ package com.challenge.movie.viewModels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import com.challenge.movie.commands.Event
+import com.challenge.movie.commands.SearchCommand
 import com.challenge.movie.db.MoviesDb
 import com.challenge.movie.db.ioThread
 import timber.log.Timber
 
 class MainViewModel(app: Application) : AndroidViewModel(app) {
+    val mSearchCommand: MutableLiveData<Event<SearchCommand>> = MutableLiveData()
     private val dao = MoviesDb.get(app).moviesDao()
     val allMovies = Pager(
         PagingConfig(
@@ -23,7 +27,9 @@ class MainViewModel(app: Application) : AndroidViewModel(app) {
     fun doSearch(tempTitle: String) {
         ioThread {
             val movies = dao.searchForMovie(tempTitle)
+            val moviesGroupedByYear = movies.groupBy { it.year }
             Timber.i("listCount: ${movies.size}")
+            mSearchCommand.postValue(Event(SearchCommand.SubmitData(moviesGroupedByYear)))
         }
     }
 }
